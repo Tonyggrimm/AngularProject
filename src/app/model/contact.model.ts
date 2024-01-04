7f71843fb581d669c3e90babfc271074bae07662
Tony Grimm <uvoix66@hotmail.com>
2024-01-05 00:34:48 +0100

    Changed the class-infrastructure to interfaces in all three models

--- src/app/model/contact.model.ts
+++ src/app/model/contact.model.ts
@@ -1,4 +1,4 @@
-export class ContactModel{
+export interface ContactModel{
   id:number;
   activites:string;
   nomPersonne:string;
@@ -8,18 +8,4 @@ export class ContactModel{
   facebook: string;
   photoUrl:string;
   email:string;
-
-
-  constructor(id:number,activites:string, nomPersonne:string,prenomPersonne: string,identifiantIG: string,numTel : number,facebook: string, photoUrl:string, email:string
-  ) {
-    this.id=id;
-    this.nomPersonne=nomPersonne;
-    this.prenomPersonne=prenomPersonne;
-    this.identifiantIG=identifiantIG;
-    this.numTel=numTel;
-    this.facebook=facebook;
-    this.photoUrl=photoUrl;
-    this.email=email;
-    this.activites=activites;
-  }
 }
--- src/app/model/destination.model.ts
+++ src/app/model/destination.model.ts
@@ -1,4 +1,4 @@
-export class destination {
+export interface destination {
   id: number;
   nomlieu: string;
   pays: string;
@@ -9,24 +9,5 @@ export class destination {
   like: number;
   note?: number;
   galerie:string [];
-
-  constructor(id: number, nomlieu: string, pays: string, continent: string, region: string,prix:number,  afficheUrl: string, like: number, galerie:string[], note?: number) {
-    this.id = id;
-    this.nomlieu = nomlieu;
-    this.pays=pays;
-    this.continent=continent;
-    this.region=region;
-
-    this.prix=prix;
-    this.afficheUrl=afficheUrl;
-
-    this.like = like;
-    this.galerie=galerie;
-    this.note = note;
-
-
-
-
-  }
 }
 
