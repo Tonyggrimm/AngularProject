3c7b7b1d3b8e4db53cb4f960a084a4039084c75c
Tony Grimm <uvoix66@hotmail.com>
2024-01-05 00:12:48 +0100

    Deleted unused and dangerous code from sidebarComponent

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -7,48 +7,4 @@ import {DestinationService} from "../services/destination.service";
   templateUrl: './sidebar.component.html',
 })
 export class SidebarComponent {
-  isChecked1!:boolean;
-  isChecked2!:boolean;
-
-
-
-
-  url!:string;
-  constructor(private route:Router, public destinationss:DestinationService) {}
-  ngOnInit(): void {
-
-    this.destinationss.isChecked1=this.isChecked1;
-
-    //console.log(this.isChecked2);
-
-
-
-  }
-  checkboxFunction() {
-    let element1 = <HTMLInputElement>document.getElementById("defaultCheck1");
-   let element2 = <HTMLInputElement>document.getElementById("defaultCheck2");
-
-
-
-
-    this.isChecked1=element1.checked;
-
-    this.destinationss.isChecked1=this.isChecked1;
-
-    this.isChecked2=element2.checked;
-    this.destinationss.isChecked2=this.isChecked2;
-    //console.log(this.destinationss.isChecked2);
-
-
-
-
-
-
-
-
-
-
-  }
-
-
 }
